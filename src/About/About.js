e68c1ab8449fa7f41a496b5ab0fc188dc16cbb91
Afsar Ali <afsarali273@gmail.com>
2021-08-22 21:28:29 +0800

    add fiiles

---
+++ src/About/About.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Header from "../Header/Header";
+
+export default class About extends Component{
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                About me Page !
+                To be Added...
+            </div>
+        );
+    }
+}
---
+++ src/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Header from "../Header/Header";
+
+export default class Contact extends Component{
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                This is Contact Me Page !! To be added
+            </div>
+        );
+    }
+}
