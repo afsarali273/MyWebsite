e3398ccafc8aabee7c7b0fe3e5130994a8d404c1
Afsar Ali <afsarali273@gmail.com>
2022-02-20 13:06:17 +0800

    add email

---
+++ src/Data/emailkey.js
@@ -0,0 +1,6 @@
+export default {
+    USER_ID: `user_bhMWx1n7x6RXkLAm6fpg2`, //userID
+    TEMPLATE_ID: `template_0m91djs`,         //templateID
+    YOUTUBE_PUBLIC_KEY:`AIzaSyDcxllPRz1le19GjTXCukLAyZICReLBaxU`
+}
+
